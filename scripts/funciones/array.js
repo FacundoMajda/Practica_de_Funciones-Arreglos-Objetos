043c9ec172203674cf82df2cc193b76c2e297d8c
FacundoMajda <facundomajda13@gmail.com>
2023-05-24 19:32:21 -0400

    m

--- scripts/funciones/array.js
+++ scripts/funciones/array.js
@@ -1,5 +1,5 @@
 //Ejercicio 1 Obtener Suma
-let numeros = [1, 2, 3, 4, 5];
+// let numeros = [1, 2, 3, 4, 5];
 function obtenerSuma(arreglo) {
   let suma = 0;
   for (let i = 0; i < arreglo.length; i++) {
@@ -9,8 +9,7 @@ function obtenerSuma(arreglo) {
 }
 console.log(obtenerSuma(numeros)); // debería devolver 15
 
-//Ejercicio 2
-
+//Ejercicio 2 Numeros pares de un array
 function obtenerPares(arreglo) {
   let pares = [];
   for (let i = 0; i < arreglo.length; i++) {
@@ -21,5 +20,35 @@ function obtenerPares(arreglo) {
   return pares;
 }
 
-let numeros = [1, 3, 4, 6, 7, 99, 8, 10, 12];
+// let numeros = [1, 3, 4, 6, 7, 99, 8, 10, 12];
 console.log(obtenerPares(numeros));
+
+//Ejercicio 3 promedio ponderado (???)
+function obtenerPromedioPonderado(notas, pesos) {
+  if (notas.length !== pesos.length) {
+    throw new Error("Los arreglos deben tener la misma longitud");
+  }
+  var sumaProductos = 0;
+  var sumaPesos = 0;
+  for (var i = 0; i < notas.length; i++) {
+    sumaProductos += notas[i] * pesos[i];
+    sumaPesos += pesos[i];
+  }
+  var promedioPonderado = sumaProductos / sumaPesos;
+
+  return promedioPonderado;
+}
+
+//Ejercicio 4
+function obtenerMaximo(arreglo) {
+  var maximo = arreglo[0];
+
+  for (var i = 1; i < arreglo.length; i++) {
+    if (arreglo[i] > maximo) {
+      maximo = arreglo[i];
+    }
+  }
+
+  return maximo;
+}
+console.log("El número máximo es: " + maximoNumero);
